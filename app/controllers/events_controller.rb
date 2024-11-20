834cfdb604c75819bec6ab0ffe46285c85414238
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-11-20 12:25:38 -0600

    host name is appearing on event details page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,6 +15,7 @@ class EventsController < ApplicationController
   # GET /events/1 or /events/1.json
   def show
     @event = Event.find(params[:id])
+    @host = User.find(@event.host_id)
   end
 
   # GET /events/new
