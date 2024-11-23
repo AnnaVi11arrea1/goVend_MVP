7e5a79fc6e4efd8d2b88db4b8bad07a5fe9388f3
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-11-23 06:48:13 +0000

    follow requests need work

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -1,5 +1,6 @@
 class FollowRequestsController < ApplicationController
   before_action :set_follow_request, only: %i[ show edit update destroy ]
+  before_action :follow_request_params, only: %i[ create update ]
 
   # GET /follow_requests or /follow_requests.json
   def index
