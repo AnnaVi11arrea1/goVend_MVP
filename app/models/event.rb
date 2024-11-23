9a7cad5f4b6322ba12f9f388dab80274ce272047
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-11-23 02:20:50 +0000

    added a ton of scopes to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -18,16 +18,6 @@
 #  host_id            :integer
 #
 class Event < ApplicationRecord
-  has_many :users, through: :vendor_events
-  has_many :vendor_events, dependent: :destroy
-
-  has_one :host, class_name: 'User', foreign_key: 'host_id'
-
-
-  def self.ransackable_attributes(auth_object = nil)
-    %w[name address tags started_at]
-  end
-
   validates :name, presence: true
   validates :application_due_at, presence: true
   validates :started_at, presence: true
@@ -35,8 +25,33 @@ class Event < ApplicationRecord
   validates :application_link, presence: true
   validates :tags, presence: true
   validates :address, presence: true
+  
+  has_many :users, through: :vendor_events
+  has_many :vendor_events, dependent: :destroy
 
+  has_one :host, class_name: 'User', foreign_key: 'host_id'
+  has_one_attached :photo
+  mount_uploader :photo, PhotoUploader
 
+  scope :past_week, -> {where(:created_at: 1.week.ago...) }
+  scope :past_month, -> {where(:created_at: 1.month.ago...) }
+  scope :past_three_months, -> {where(:created_at: 3.months.ago...) }
+  scope :past_six_months, -> {where(:created_at: 6.months.ago...) }
+  scope :past_year, -> {where(:created_at: 1.year.ago...) }
+
+  scope :future, -> {where(:started_at: Date.today..) }
+  scope :happening_soon, -> {where(:started_at: Date.today..1.week.from_now) }
+  scope :this_month, -> {where(:started_at: Date.today.beginning_of_month..Date.today.end_of_month) }
+  scope :next_three_months,{where(:started_at: Date.today..3.months.from_now) }
+
+  scope :by_host, -> (host_id) { where(host_id: host_id) }
+  scope :by_tags, -> (tags) { where(tags: tags) }
+  scope :by_date, -> (date) { where(started_at: date) }
+
+  
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name address tags started_at]
+  end
 
   def self.ransackable_associations(auth_object = nil)
     []
@@ -58,7 +73,5 @@ class Event < ApplicationRecord
     end
   end
 
-  has_one_attached :photo
-  mount_uploader :photo, PhotoUploader
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@
 #  last_name              :string
 #  locked_at              :datetime
 #  photo                  :string
+#  private                :boolean          default(FALSE)
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
@@ -32,23 +33,26 @@
 #  index_users_on_unlock_token          (unlock_token) UNIQUE
 #
 class User < ApplicationRecord
-  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-
-  has_many :vendor_events
-  has_many :events, through: :vendor_events
-
-
-
   validates :username, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
 
+  has_many :vendor_events
+  has_many :events, through: :vendor_events
+  has_many :sent_follow_requests, foreign_key: :recipient_id, class_name: 'FollowRequest'
+  has_many :received_follow_requests, foreign_key: :sender_id, class_name: 'FollowRequest'
+  has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
+  has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :followers, through: :accepted_received_follow_requests, source: :sender
+  has_many :feed, through: :leaders, source: :events
+  has_many :discover, through: :leaders, source: :followers # events of the leaders followers 
 
- 
   has_one_attached :photo 
 
   mount_uploader :photo, PhotoUploader
