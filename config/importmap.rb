dcade3dd57555b17237aac081f114fda1f12fcf6
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-12-03 23:00:20 +0000

    trying to fix errors in production

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,9 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@rails/ujs", to: "@rails--ujs.js" # @7.0.8
-pin "jquery" # @3.7.1
-pin "bootstrap" # @5.3.0
+pin "@rails/ujs", to: "https://cdnjs.cloudflare.com/ajax/libs/rails-ujs/6.1.4/rails-ujs.min.js"
+pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js"
 pin_all_from "app/javascript/helpers", under: "helpers"
-pin "application", to: "application.js"
-pin "initMap", to: "path/to/initMap.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
