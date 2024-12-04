cb171a0c3c47d300ef4712cd0a59691eb0ae1588
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-12-04 15:27:48 +0000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,18 +15,13 @@ Rails.application.routes.draw do
     end
   end
   resources :users
-  resources "/charts", => "charts#index"
-
-
+  
   get ":username/feed" => "users#feed", as: :feed
   get ":username/followers" => "users#followers", as: :followers
   get ":username/following" => "users#following", as: :following
   get ":username/discover" => "users#discover", as: :discover
-
-
+  
+  get "/charts" => "charts#index"
   get "/logout" => "users#logout"
   get "/privacy_policy" => "users#privacy", as: :privacy_policy
-
-
-
 end
