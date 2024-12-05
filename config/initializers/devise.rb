4c270485214297d6bbc8e0cf34a84f282c418529
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-12-05 18:40:42 +0000

    commented out a line i wasnt using

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, ENV.fetch("HUB_ID"), ENV.fetch("HUB_SECRET"), scope: "user"
+  # config.omniauth :github, ENV.fetch("HUB_ID"), ENV.fetch("HUB_SECRET"), scope: "user"
 
 
   # ==> Warden configuration
