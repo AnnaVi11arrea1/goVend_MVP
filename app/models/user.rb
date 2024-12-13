757c2a3dec14f586f91f1ac32e6d93118f9de7d6
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-12-13 01:26:39 +0000

    fun buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,8 +43,8 @@ class User < ApplicationRecord
   has_many :vendor_events
   has_many :hosted_events, class_name: 'Event', foreign_key: 'host_id'
   has_many :events, through: :vendor_events
-  has_many :sent_follow_requests, foreign_key: :recipient_id, class_name: 'FollowRequest'
-  has_many :received_follow_requests, foreign_key: :sender_id, class_name: 'FollowRequest'
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: 'FollowRequest'
+  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: 'FollowRequest'
   has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
 
