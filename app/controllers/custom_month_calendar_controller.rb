8f1fd212246c69cd8ac6e9cf12abf1e445f06bc4
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-12-23 16:47:34 +0000

    modded controller

--- app/controllers/custom_month_calendar_controller.rb
+++ app/controllers/custom_month_calendar_controller.rb
@@ -1,4 +1,4 @@
-class CustomMonthCalendar < SimpleCalendar::MonthCalendar
+class CustomMonthCalendarController < SimpleCalendar::MonthCalendar
   def render(&block)
     content_tag(:div, class: options[:month_header_class]) do
       super(&block)
